ee2f1b7557eafa76a8d1fdfdf03a9f990f03a2d7
Jen' <Jen'@Jennifer>
2016-11-24 08:52:40 +0100

    Suite du travail de Séverin sur la création des rooms

--- src/com/gp2/GameInstanciation/RoomCreation.java
+++ src/com/gp2/GameInstanciation/RoomCreation.java
@@ -17,19 +17,48 @@ public class RoomCreation {
 	public Room cuisine;
 	
 	public RoomCreation(){
+		
 		//tour
-		tour = new Room("tour","Salle initiale oÃ¹ se trouve notre licorne");
+		tour = new Room("tour","Salle initiale ou se trouve notre licorne");
 		tour.addExit("DOWN", new Exit(tour,cour));
 		tour.addExit("WEST", new Exit(cour,prison));
+		
 		//cour
 		cour = new Room("cour","cour principale du chateau");
 		cour.addExit("EAST", new Exit(cour,ecurie));
 		cour.addExit("WEST", new Exit(cour,salleBanquet));
-		cour.addExit("NORTH", new Exit(cour,reserve));
+		cour.addExit("DOWN", new Exit(cour,reserve));
+		
 		//reserve
 		reserve = new Room("reserve","");
-		reserve.addExit("EAST", new Exit(reserve,cour));
+		reserve.addExit("UP", new Exit(reserve,cour));
 		
+		//ecurie
+		ecurie = new Room("Ecurie","Dans cette ecurie il y a des chevaux");
+		ecurie.addExit("SOUTH", new Exit(ecurie,cour));
+		
+		//salle banquet 
+		salleBanquet = new Room ("Salle du banquet","Salle de banquet avec beaucoup beaucoup beaucoup de personnes");
+		salleBanquet.addExit("EAST",new Exit(salleBanquet,vestibule));
+		salleBanquet.addExit("SOUTH",new Exit(salleBanquet,cuisine));
+		salleBanquet.addExit("WEST",new Exit(salleBanquet,cour));
+		
+		//Salle trone 
+		salleTrone = new Room ("Salle du trone","La ou la méchante reine vous attend");
+		salleTrone.addExit("WEST", new Exit (salleTrone,vestibule));
+		
+		//Vestibule 
+		vestibule = new Room ("Vestibule","Les gardes sont la pour botter ton petiit cul");
+		vestibule.addExit("EAST", new Exit (vestibule, salleTrone));
+		vestibule.addExit("WEST", new Exit (vestibule, salleBanquet));
+		
+		//Prison 
+		prison = new Room ("Prison","Ceci est la piece ou tu perd tout et tu pleure!!!"); 
+		prison.addExit("WEST", new Exit (prison, tour));
+		
+		//cuisine
+		cuisine = new Room ("Cuisine","Ceci est la piece ou tout sens bon et ou tu peut prendre des kilos");
+		cuisine.addExit("NORTH", new Exit (cuisine,salleBanquet));	
 		
 	}
 	
