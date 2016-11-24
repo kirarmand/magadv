1039df9186ac3f7105fb0e619112daab67b0d090
Severin <lhommeletseverin@gmail.com>
2016-11-24 10:00:11 +0100

    Classe roomCreation fini ! Il manque juste un item (key) dans la classe exitCreation

---
+++ src/com/gp2/GameInstanciation/ExitCreation.java
@@ -0,0 +1,77 @@
+package com.gp2.GameInstanciation;
+import com.gp2.component.*;
+import java.util.ArrayList;
+
+public class ExitCreation {
+	private Exit Door_TC;
+	private Exit Door_PT;
+	
+	private Exit Door_CT;
+	private ExitWithItem Door_CE;
+	private Exit Door_CsB;
+	private Exit Door_CR;
+	
+	private Exit Door_EC;
+	
+	private Exit Door_RC;
+	
+	private Exit Door_sBC;
+	private Exit Door_sBV;
+	private Exit Door_sBCu;
+	
+	private Exit Door_CusB;
+	
+	private Exit Door_VsB;
+	private Exit Door_VsT;
+	
+	private Exit Door_sTV;
+	
+	private RoomCreation roomCr;
+	private Item unlockItem;
+	private ArrayList<Exit> exitList;
+	
+	public ExitCreation(){
+		Door_TC = new Exit(roomCr.getAllRooms().get(0),roomCr.getAllRooms().get(1));
+		exitList.add(Door_TC);
+		Door_PT = new Exit(roomCr.getAllRooms().get(7),roomCr.getAllRooms().get(0));
+		exitList.add(Door_PT);
+		
+		Door_CT = new Exit(roomCr.getAllRooms().get(1),roomCr.getAllRooms().get(0));
+		exitList.add(Door_CT);
+		Door_CE = new ExitWithItem(roomCr.getAllRooms().get(1),roomCr.getAllRooms().get(3),unlockItem);
+		exitList.add(Door_CE);
+		Door_CsB = new Exit(roomCr.getAllRooms().get(1),roomCr.getAllRooms().get(4));
+		exitList.add(Door_CsB);
+		Door_CR = new Exit(roomCr.getAllRooms().get(1),roomCr.getAllRooms().get(2));
+		exitList.add(Door_CR);
+		
+		Door_EC = new Exit(roomCr.getAllRooms().get(0),roomCr.getAllRooms().get(1));
+		exitList.add(Door_EC);
+		
+		Door_RC = new Exit(roomCr.getAllRooms().get(3),roomCr.getAllRooms().get(1));
+		exitList.add(Door_RC);
+		
+		Door_sBC = new Exit(roomCr.getAllRooms().get(4),roomCr.getAllRooms().get(1));
+		exitList.add(Door_sBC);
+		Door_sBV = new Exit(roomCr.getAllRooms().get(4),roomCr.getAllRooms().get(6));
+		exitList.add(Door_sBV);
+		Door_sBCu = new Exit(roomCr.getAllRooms().get(4),roomCr.getAllRooms().get(8));
+		exitList.add(Door_sBCu);
+		
+		Door_CusB = new Exit(roomCr.getAllRooms().get(8),roomCr.getAllRooms().get(4));
+		exitList.add(Door_CusB);
+		
+		Door_VsB = new Exit(roomCr.getAllRooms().get(6),roomCr.getAllRooms().get(4));
+		exitList.add(Door_VsB);
+		Door_VsT = new Exit(roomCr.getAllRooms().get(6),roomCr.getAllRooms().get(5));
+		exitList.add(Door_VsT);
+		
+		Door_sTV = new Exit(roomCr.getAllRooms().get(5),roomCr.getAllRooms().get(6));
+		exitList.add(Door_sTV);
+	}
+	
+	public ArrayList<Exit> getAllDoor(){
+		return exitList;
+	}
+}
+
--- src/com/gp2/GameInstanciation/RoomCreation.java
+++ src/com/gp2/GameInstanciation/RoomCreation.java
@@ -16,59 +16,60 @@ public class RoomCreation {
 	private Room prison;
 	private Room cuisine;
 	private ArrayList<Room> listRoom; 
+	private ExitCreation roomList;
 	
 	
 	public RoomCreation(){
 		
 		//tour
 		tour = new Room("tour","Salle initiale ou se trouve notre licorne");
-		tour.addExit("DOWN", new Exit(tour,cour));
-		tour.addExit("EAST", new Exit(cour,prison));
+		tour.addExit("DOWN", roomList.getAllDoor().get(0));
 		listRoom.add(tour);
 		
 		//cour
 		cour = new Room("cour","cour principale du chateau");
-		cour.addExit("NORTH", new Exit(cour,ecurie));
-		cour.addExit("EAST", new Exit(cour,salleBanquet));
-		cour.addExit("DOWN", new Exit(cour,reserve));
+		cour.addExit("UP", roomList.getAllDoor().get(2));
+		cour.addExit("NORTH", roomList.getAllDoor().get(3));
+		cour.addExit("EAST", roomList.getAllDoor().get(4));
+		cour.addExit("DOWN", roomList.getAllDoor().get(5));
 		listRoom.add(cour);
 		
 		//reserve
 		reserve = new Room("reserve","");
-		reserve.addExit("UP", new Exit(reserve,cour));
+		reserve.addExit("UP", roomList.getAllDoor().get(7));
 		listRoom.add(reserve);
 		
 		//ecurie
 		ecurie = new Room("Ecurie","Dans cette ecurie il y a des chevaux");
-		ecurie.addExit("SOUTH", new Exit(ecurie,cour));
+		ecurie.addExit("SOUTH", roomList.getAllDoor().get(6));
 		listRoom.add(ecurie);
 		
 		//salle banquet 
 		salleBanquet = new Room ("Salle du banquet","Salle de banquet avec beaucoup beaucoup beaucoup de personnes");
-		salleBanquet.addExit("EAST",new Exit(salleBanquet,vestibule));
-		salleBanquet.addExit("SOUTH",new Exit(salleBanquet,cuisine));
-		salleBanquet.addExit("WEST",new Exit(salleBanquet,cour));
+		salleBanquet.addExit("EAST",roomList.getAllDoor().get(9));
+		salleBanquet.addExit("SOUTH",roomList.getAllDoor().get(10));
+		salleBanquet.addExit("WEST",roomList.getAllDoor().get(8));
 		listRoom.add(salleBanquet);
 		
 		//Salle trone 
-		salleTrone = new Room ("Salle du trone","La ou la méchante reine vous attend");
-		salleTrone.addExit("WEST", new Exit (salleTrone,vestibule));
+		salleTrone = new Room ("Salle du trone","La ou la mechante reine vous attend");
+		salleTrone.addExit("WEST", roomList.getAllDoor().get(14));
 		listRoom.add(salleTrone);
 		
 		//Vestibule 
 		vestibule = new Room ("Vestibule","Les gardes sont la pour botter ton petiit cul");
-		vestibule.addExit("EAST", new Exit (vestibule, salleTrone));
-		vestibule.addExit("WEST", new Exit (vestibule, salleBanquet));
+		vestibule.addExit("EAST", roomList.getAllDoor().get(13));
+		vestibule.addExit("WEST", roomList.getAllDoor().get(12));
 		listRoom.add(vestibule);
 		
 		//Prison 
 		prison = new Room ("Prison","Ceci est la piece ou tu perd tout et tu pleure!!!"); 
-		prison.addExit("WEST", new Exit (prison, tour));
+		prison.addExit("WEST", roomList.getAllDoor().get(1));
 		listRoom.add(prison);
 		
 		//cuisine
 		cuisine = new Room ("Cuisine","Ceci est la piece ou tout sens bon et ou tu peut prendre des kilos");
-		cuisine.addExit("NORTH", new Exit (cuisine,salleBanquet));	
+		cuisine.addExit("NORTH", roomList.getAllDoor().get(11));	
 		listRoom.add(cuisine);
 	}
 	
