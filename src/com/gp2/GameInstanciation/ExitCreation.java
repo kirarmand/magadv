564943f129f7f9c2036d29255977aa973016eb11
Jen' <Jen'@Jennifer>
2016-11-24 11:51:03 +0100

    Commentaires sur les classes ExitCreation et RoomCreation

--- src/com/gp2/GameInstanciation/ExitCreation.java
+++ src/com/gp2/GameInstanciation/ExitCreation.java
@@ -2,6 +2,11 @@ package com.gp2.GameInstanciation;
 import com.gp2.component.*;
 import java.util.ArrayList;
 
+/**
+ * This class create the different doors of the game
+ * @author group2
+ *
+ */
 public class ExitCreation {
 	
 	// Toutes les sorties du jeu
@@ -38,43 +43,78 @@ public class ExitCreation {
 		// 0 correspond à la room au rang 0 dans le tableau listRoom cree dans la class RoomCreation : cour. 
 		// 1 correspond à la room au rang 1 dans le tableau listRoom cree dans la class RoomCreation : tour. 
 		Door_TC = new Exit(roomCr.getAllRooms().get(0),roomCr.getAllRooms().get(1));
+		//Ajout de la porte au tableau
 		exitList.add(Door_TC);
+		
 		// Porte entre la prison et la tour
 		Door_PT = new Exit(roomCr.getAllRooms().get(7),roomCr.getAllRooms().get(0));
+		//Ajout de la porte au tableau
 		exitList.add(Door_PT);
 		
+		//porte entre la cour et la tour
 		Door_CT = new Exit(roomCr.getAllRooms().get(1),roomCr.getAllRooms().get(0));
+		//Ajout de la porte au tableau
 		exitList.add(Door_CT);
+		
+		// porte entre la cour et l'ecurie 
 		// Porte qui necessite un item
 		Door_CE = new ExitWithItem(roomCr.getAllRooms().get(1),roomCr.getAllRooms().get(3),unlockItem.getKey());
+		//Ajout de la porte au tableau
 		exitList.add(Door_CE);
+		
+		// porte entre la cour et la salle de banquet
 		Door_CsB = new Exit(roomCr.getAllRooms().get(1),roomCr.getAllRooms().get(4));
+		//Ajout de la porte au tableau
 		exitList.add(Door_CsB);
+		
+		// porte entre la cour et la reserve
 		Door_CR = new Exit(roomCr.getAllRooms().get(1),roomCr.getAllRooms().get(2));
+		//Ajout de la porte au tableau
 		exitList.add(Door_CR);
 		
+		// porte entre l'ecurie et la cour
 		Door_EC = new Exit(roomCr.getAllRooms().get(0),roomCr.getAllRooms().get(1));
+		//Ajout de la porte au tableau
 		exitList.add(Door_EC);
 		
+		// porte entre la reserve et la cour 
 		Door_RC = new Exit(roomCr.getAllRooms().get(3),roomCr.getAllRooms().get(1));
+		//Ajout de la porte au tableau
 		exitList.add(Door_RC);
 		
+		// porte entre la salle de banquet et la cour 
 		Door_sBC = new Exit(roomCr.getAllRooms().get(4),roomCr.getAllRooms().get(1));
+		//Ajout de la porte au tableau
 		exitList.add(Door_sBC);
+		
+		// porte entre la salle de banquet et le vestibule
 		Door_sBV = new Exit(roomCr.getAllRooms().get(4),roomCr.getAllRooms().get(6));
+		//Ajout de la porte au tableau
 		exitList.add(Door_sBV);
+		
+		// porte entre la salle de banquet et la cuisine
 		Door_sBCu = new Exit(roomCr.getAllRooms().get(4),roomCr.getAllRooms().get(8));
+		//Ajout de la porte au tableau
 		exitList.add(Door_sBCu);
 		
+		// porte entre la cuisine et la salle de  banquet
 		Door_CusB = new Exit(roomCr.getAllRooms().get(8),roomCr.getAllRooms().get(4));
+		//Ajout de la porte au tableau
 		exitList.add(Door_CusB);
 		
+		// porte entre le vestibule et la salle de banquet 
 		Door_VsB = new Exit(roomCr.getAllRooms().get(6),roomCr.getAllRooms().get(4));
+		//Ajout de la porte au tableau
 		exitList.add(Door_VsB);
+		
+		// porte entre le vestibule et la salle de trone
 		Door_VsT = new Exit(roomCr.getAllRooms().get(6),roomCr.getAllRooms().get(5));
+		//Ajout de la porte au tableau
 		exitList.add(Door_VsT);
 		
+		// porte entre la salle de trone et le vestibule
 		Door_sTV = new Exit(roomCr.getAllRooms().get(5),roomCr.getAllRooms().get(6));
+		//Ajout de la porte au tableau
 		exitList.add(Door_sTV);
 	}
 	
--- src/com/gp2/GameInstanciation/RoomCreation.java
+++ src/com/gp2/GameInstanciation/RoomCreation.java
@@ -2,6 +2,11 @@ package com.gp2.GameInstanciation;
 import com.gp2.component.*;
 import java.util.ArrayList;
 
+/**
+ * This class create the different rooms of the game
+ * @author group2
+ *
+ */
 public class RoomCreation {
 	
 // Attributes which describe each room in the game
@@ -15,61 +20,81 @@ public class RoomCreation {
 	private Room vestibule;
 	private Room prison;
 	private Room cuisine;
+	
+	// Tableau qui contiendra toutes les pieces du jeu
 	private ArrayList<Room> listRoom; 
 	private ExitCreation roomList;
 	
 	
 	public RoomCreation(){
 		
-		//tour
+		//Creation de la tour
 		tour = new Room("tour","Salle initiale ou se trouve notre licorne");
+		//Ajout des portes a la piece
 		tour.addExit("DOWN", roomList.getAllDoor().get(0));
+		//Ajout de la piece au tableau
 		listRoom.add(tour);
 		
-		//cour
+		//Creation de la cour
 		cour = new Room("cour","cour principale du chateau");
+		//Ajout des portes a la piece
 		cour.addExit("UP", roomList.getAllDoor().get(2));
 		cour.addExit("NORTH", roomList.getAllDoor().get(3));
 		cour.addExit("EAST", roomList.getAllDoor().get(4));
 		cour.addExit("DOWN", roomList.getAllDoor().get(5));
+		//Ajout de la piece au tableau
 		listRoom.add(cour);
 		
-		//reserve
+		// Creation de la reserve
 		reserve = new Room("reserve","");
+		//Ajout des portes a la piece
 		reserve.addExit("UP", roomList.getAllDoor().get(7));
+		//Ajout de la piece au tableau
 		listRoom.add(reserve);
 		
-		//ecurie
+		//Creation de l'ecurie
 		ecurie = new Room("Ecurie","Dans cette ecurie il y a des chevaux");
+		//Ajout des portes a la piece
 		ecurie.addExit("SOUTH", roomList.getAllDoor().get(6));
+		//Ajout de la piece au tableau
 		listRoom.add(ecurie);
 		
-		//salle banquet 
+		//Creation de la salle banquet 
 		salleBanquet = new Room ("Salle du banquet","Salle de banquet avec beaucoup beaucoup beaucoup de personnes");
+		//Ajout des portes a la piece
 		salleBanquet.addExit("EAST",roomList.getAllDoor().get(9));
 		salleBanquet.addExit("SOUTH",roomList.getAllDoor().get(10));
 		salleBanquet.addExit("WEST",roomList.getAllDoor().get(8));
+		//Ajout de la piece au tableau
 		listRoom.add(salleBanquet);
 		
-		//Salle trone 
+		//Creation de la Salle trone 
 		salleTrone = new Room ("Salle du trone","La ou la mechante reine vous attend");
+		//Ajout des portes a la piece
 		salleTrone.addExit("WEST", roomList.getAllDoor().get(14));
+		//Ajout de la piece au tableau
 		listRoom.add(salleTrone);
 		
-		//Vestibule 
+		//Creation du Vestibule 
 		vestibule = new Room ("Vestibule","Les gardes sont la pour botter ton petiit cul");
+		//Ajout des portes a la piece
 		vestibule.addExit("EAST", roomList.getAllDoor().get(13));
 		vestibule.addExit("WEST", roomList.getAllDoor().get(12));
+		//Ajout de la piece au tableau
 		listRoom.add(vestibule);
 		
-		//Prison 
+		//Creation de la Prison 
 		prison = new Room ("Prison","Ceci est la piece ou tu perd tout et tu pleure!!!"); 
+		//Ajout des portes a la piece
 		prison.addExit("WEST", roomList.getAllDoor().get(1));
+		//Ajout de la piece au tableau
 		listRoom.add(prison);
 		
-		//cuisine
+		//Creation de la cuisine
 		cuisine = new Room ("Cuisine","Ceci est la piece ou tout sens bon et ou tu peut prendre des kilos");
-		cuisine.addExit("NORTH", roomList.getAllDoor().get(11));	
+		//Ajout des portes a la piece
+		cuisine.addExit("NORTH", roomList.getAllDoor().get(11));
+		//Ajout de la piece au tableau
 		listRoom.add(cuisine);
 	}
 	
