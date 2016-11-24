8d94eedc57e3501b3df886cf35946fd7cfe915d3
Jen' <Jen'@Jennifer>
2016-11-24 11:13:22 +0100

    Commentaire sur la classe ExitCreation

--- src/com/gp2/GameInstanciation/ExitCreation.java
+++ src/com/gp2/GameInstanciation/ExitCreation.java
@@ -3,41 +3,49 @@ import com.gp2.component.*;
 import java.util.ArrayList;
 
 public class ExitCreation {
-	private Exit Door_TC;
-	private Exit Door_PT;
 	
-	private Exit Door_CT;
-	private ExitWithItem Door_CE;
-	private Exit Door_CsB;
-	private Exit Door_CR;
+	// Toutes les sorties du jeu
+	private Exit Door_TC; // porte entre la tour et la cour 
+	private Exit Door_PT;  // porte entre la prison et la tour
 	
-	private Exit Door_EC;
+	private Exit Door_CT; //porte entre la cour et la tour
+	private ExitWithItem Door_CE; // porte entre la cour et l'ecurie 
+	private Exit Door_CsB; // porte entre la cour et la salle de banquet 
+	private Exit Door_CR; // porte entre la cour et la reserve
 	
-	private Exit Door_RC;
+	private Exit Door_EC;  // porte entre l'ecurie et la cour 
 	
-	private Exit Door_sBC;
-	private Exit Door_sBV;
-	private Exit Door_sBCu;
+	private Exit Door_RC;  // porte entre la reserve et la cour 
 	
-	private Exit Door_CusB;
+	private Exit Door_sBC;  // porte entre la salle de banquet et la cour 
+	private Exit Door_sBV;  // porte entre la salle de banquet et le vestibule
+	private Exit Door_sBCu;  // porte entre la salle de banquet et la cuisine
 	
-	private Exit Door_VsB;
-	private Exit Door_VsT;
+	private Exit Door_CusB;  // porte entre la cuisine et la salle de  banquet
 	
-	private Exit Door_sTV;
+	private Exit Door_VsB;  // porte entre le vestibule et la salle de banquet 
+	private Exit Door_VsT;  // porte entre le vestibule et la salle de trone
 	
-	private RoomCreation roomCr;
-	private ItemCreation unlockItem;
-	private ArrayList<Exit> exitList;
+	private Exit Door_sTV;  // porte entre la salle de trone et le vestibule
 	
+	private RoomCreation roomCr; // Repuration des rooms crees dans la classe RoomCreation
+	private ItemCreation unlockItem; // Recuperation des items crees dans la classe ItemCreation
+	private ArrayList<Exit> exitList; // Tableau contenant tout les exits
+	
+	//Creation des differentes sorties du jeu
 	public ExitCreation(){
+		// creation de la porte entre la tour et la cour. 
+		// 0 correspond à la room au rang 0 dans le tableau listRoom cree dans la class RoomCreation : cour. 
+		// 1 correspond à la room au rang 1 dans le tableau listRoom cree dans la class RoomCreation : tour. 
 		Door_TC = new Exit(roomCr.getAllRooms().get(0),roomCr.getAllRooms().get(1));
 		exitList.add(Door_TC);
+		// Porte entre la prison et la tour
 		Door_PT = new Exit(roomCr.getAllRooms().get(7),roomCr.getAllRooms().get(0));
 		exitList.add(Door_PT);
 		
 		Door_CT = new Exit(roomCr.getAllRooms().get(1),roomCr.getAllRooms().get(0));
 		exitList.add(Door_CT);
+		// Porte qui necessite un item
 		Door_CE = new ExitWithItem(roomCr.getAllRooms().get(1),roomCr.getAllRooms().get(3),unlockItem.getKey());
 		exitList.add(Door_CE);
 		Door_CsB = new Exit(roomCr.getAllRooms().get(1),roomCr.getAllRooms().get(4));
