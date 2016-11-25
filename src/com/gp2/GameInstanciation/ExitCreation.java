12e952d52cad7b3b002adfbdcd526794f6821bd2
Severin <lhommeletseverin@gmail.com>
2016-11-25 10:14:41 +0100

    ExitCreation done !

--- src/com/gp2/GameInstanciation/ExitCreation.java
+++ src/com/gp2/GameInstanciation/ExitCreation.java
@@ -2,122 +2,100 @@ package com.gp2.GameInstanciation;
 import com.gp2.component.*;
 import java.util.ArrayList;
 
-/**
- * This class create the different doors of the game
- * @author group2
- *
- */
+// This class instantiate all exits of the program 
+// It takes in parameters a last room and a next room
+
 public class ExitCreation {
 	
-	// Toutes les sorties du jeu
-	private Exit Door_TC; // porte entre la tour et la cour 
-	private Exit Door_PT;  // porte entre la prison et la tour
+	// Door between The tower and the courtyard
+	private Exit Door_TC;
+	// Door between The jail and the tower
+	private Exit Door_JT;
 	
-	private Exit Door_CT; //porte entre la cour et la tour
-	private ExitWithItem Door_CE; // porte entre la cour et l'ecurie 
-	private Exit Door_CsB; // porte entre la cour et la salle de banquet 
-	private Exit Door_CR; // porte entre la cour et la reserve
+	// Door between The courtyard and the tower
+	private Exit Door_CT;
+	// Door between The courtyard and the stable
+	private ExitWithItem Door_CS;
+	// Door between The courtyard and the banquet hall
+	private Exit Door_CbH;
+	// Door between The courtyard and stock
+	private Exit Door_CSt;
 	
-	private Exit Door_EC;  // porte entre l'ecurie et la cour 
+	// Door between The stable and the courtyard
+	private Exit Door_SC;
+	// Door between The stock and courtyard
+	private Exit Door_StC;
 	
-	private Exit Door_RC;  // porte entre la reserve et la cour 
+	// Door between The banquet hall and the courtyard
+	private Exit Door_bHC;
+	// Door between The banquet hall and vestibule
+	private Exit Door_bHV;
+	// Door between The banquet hall and the kitchen
+	private Exit Door_bHK;
 	
-	private Exit Door_sBC;  // porte entre la salle de banquet et la cour 
-	private Exit Door_sBV;  // porte entre la salle de banquet et le vestibule
-	private Exit Door_sBCu;  // porte entre la salle de banquet et la cuisine
+	// Door between The kitchen and banquet hall
+	private Exit Door_KbH;
 	
-	private Exit Door_CusB;  // porte entre la cuisine et la salle de  banquet
+	// Door between The vestibule and the banquet hall
+	private Exit Door_VbH;
+	// Door between The vestibule and the throne hall
+	private Exit Door_VtH;
+	// Door between The throne hall and the vestibule
+	private Exit Door_tHV;
 	
-	private Exit Door_VsB;  // porte entre le vestibule et la salle de banquet 
-	private Exit Door_VsT;  // porte entre le vestibule et la salle de trone
+	private RoomCreation roomCr; // Instantiation of the class roomCreation
+	private ItemCreation unlockItem; // Instantiation of the class ItemCreation
+	private ArrayList<Exit> exitList; // Creation of list in order to stock all exits
 	
-	private Exit Door_sTV;  // porte entre la salle de trone et le vestibule
-	
-	private RoomCreation roomCr; // Repuration des rooms crees dans la classe RoomCreation
-	private ItemCreation unlockItem; // Recuperation des items crees dans la classe ItemCreation
-	private ArrayList<Exit> exitList; // Tableau contenant tout les exits
-	
-	//Creation des differentes sorties du jeu
 	public ExitCreation(){
-		// creation de la porte entre la tour et la cour. 
-		// 0 correspond à la room au rang 0 dans le tableau listRoom cree dans la class RoomCreation : cour. 
-		// 1 correspond à la room au rang 1 dans le tableau listRoom cree dans la class RoomCreation : tour. 
-		Door_TC = new Exit(roomCr.getAllRooms().get(0),roomCr.getAllRooms().get(1));
-		//Ajout de la porte au tableau
-		exitList.add(Door_TC);
 		
-		// Porte entre la prison et la tour
-		Door_PT = new Exit(roomCr.getAllRooms().get(7),roomCr.getAllRooms().get(0));
-		//Ajout de la porte au tableau
-		exitList.add(Door_PT);
+		// Creation of a new Exit in the variable Door_TC
+		Door_TC = new Exit(roomCr.getAllRooms().get(0),roomCr.getAllRooms().get(1)); 
+		// Door_TC is added in the list exitList
+		exitList.add(Door_TC); 
+		Door_JT = new Exit(roomCr.getAllRooms().get(7),roomCr.getAllRooms().get(0));
+		exitList.add(Door_JT);
 		
-		//porte entre la cour et la tour
 		Door_CT = new Exit(roomCr.getAllRooms().get(1),roomCr.getAllRooms().get(0));
-		//Ajout de la porte au tableau
 		exitList.add(Door_CT);
+		// The class instantiate is the ExitWithItem class which extends Exit class.
+		// We have to input an item with unlock this door.
+		Door_CS = new ExitWithItem(roomCr.getAllRooms().get(1),roomCr.getAllRooms().get(3),unlockItem.getKey());
+		exitList.add(Door_CS);
+		Door_CbH = new Exit(roomCr.getAllRooms().get(1),roomCr.getAllRooms().get(4));
+		exitList.add(Door_CbH);
+		Door_CSt = new Exit(roomCr.getAllRooms().get(1),roomCr.getAllRooms().get(2));
+		exitList.add(Door_CSt);
 		
-		// porte entre la cour et l'ecurie 
-		// Porte qui necessite un item
-		Door_CE = new ExitWithItem(roomCr.getAllRooms().get(1),roomCr.getAllRooms().get(3),unlockItem.getKey());
-		//Ajout de la porte au tableau
-		exitList.add(Door_CE);
+		Door_SC = new Exit(roomCr.getAllRooms().get(0),roomCr.getAllRooms().get(1));
+		exitList.add(Door_SC);
 		
-		// porte entre la cour et la salle de banquet
-		Door_CsB = new Exit(roomCr.getAllRooms().get(1),roomCr.getAllRooms().get(4));
-		//Ajout de la porte au tableau
-		exitList.add(Door_CsB);
+		Door_StC = new Exit(roomCr.getAllRooms().get(3),roomCr.getAllRooms().get(1));
+		exitList.add(Door_StC);
 		
-		// porte entre la cour et la reserve
-		Door_CR = new Exit(roomCr.getAllRooms().get(1),roomCr.getAllRooms().get(2));
-		//Ajout de la porte au tableau
-		exitList.add(Door_CR);
+		Door_bHC = new Exit(roomCr.getAllRooms().get(4),roomCr.getAllRooms().get(1));
+		exitList.add(Door_bHC);
+		Door_bHV = new Exit(roomCr.getAllRooms().get(4),roomCr.getAllRooms().get(6));
+		exitList.add(Door_bHV);
+		Door_bHK = new Exit(roomCr.getAllRooms().get(4),roomCr.getAllRooms().get(8));
+		exitList.add(Door_bHK);
 		
-		// porte entre l'ecurie et la cour
-		Door_EC = new Exit(roomCr.getAllRooms().get(0),roomCr.getAllRooms().get(1));
-		//Ajout de la porte au tableau
-		exitList.add(Door_EC);
+		Door_KbH = new Exit(roomCr.getAllRooms().get(8),roomCr.getAllRooms().get(4));
+		exitList.add(Door_KbH);
 		
-		// porte entre la reserve et la cour 
-		Door_RC = new Exit(roomCr.getAllRooms().get(3),roomCr.getAllRooms().get(1));
-		//Ajout de la porte au tableau
-		exitList.add(Door_RC);
+		Door_VbH = new Exit(roomCr.getAllRooms().get(6),roomCr.getAllRooms().get(4));
+		exitList.add(Door_VbH);
+		Door_VtH = new Exit(roomCr.getAllRooms().get(6),roomCr.getAllRooms().get(5));
+		exitList.add(Door_VtH);
 		
-		// porte entre la salle de banquet et la cour 
-		Door_sBC = new Exit(roomCr.getAllRooms().get(4),roomCr.getAllRooms().get(1));
-		//Ajout de la porte au tableau
-		exitList.add(Door_sBC);
-		
-		// porte entre la salle de banquet et le vestibule
-		Door_sBV = new Exit(roomCr.getAllRooms().get(4),roomCr.getAllRooms().get(6));
-		//Ajout de la porte au tableau
-		exitList.add(Door_sBV);
-		
-		// porte entre la salle de banquet et la cuisine
-		Door_sBCu = new Exit(roomCr.getAllRooms().get(4),roomCr.getAllRooms().get(8));
-		//Ajout de la porte au tableau
-		exitList.add(Door_sBCu);
-		
-		// porte entre la cuisine et la salle de  banquet
-		Door_CusB = new Exit(roomCr.getAllRooms().get(8),roomCr.getAllRooms().get(4));
-		//Ajout de la porte au tableau
-		exitList.add(Door_CusB);
-		
-		// porte entre le vestibule et la salle de banquet 
-		Door_VsB = new Exit(roomCr.getAllRooms().get(6),roomCr.getAllRooms().get(4));
-		//Ajout de la porte au tableau
-		exitList.add(Door_VsB);
-		
-		// porte entre le vestibule et la salle de trone
-		Door_VsT = new Exit(roomCr.getAllRooms().get(6),roomCr.getAllRooms().get(5));
-		//Ajout de la porte au tableau
-		exitList.add(Door_VsT);
-		
-		// porte entre la salle de trone et le vestibule
-		Door_sTV = new Exit(roomCr.getAllRooms().get(5),roomCr.getAllRooms().get(6));
-		//Ajout de la porte au tableau
-		exitList.add(Door_sTV);
+		Door_tHV = new Exit(roomCr.getAllRooms().get(5),roomCr.getAllRooms().get(6));
+		exitList.add(Door_tHV);
 	}
-	
+	/**
+	 * This getter method return a list which contains all exits
+	 * The aim is to use that method from another class to call exits
+	 * @return exitList 
+	 */
 	public ArrayList<Exit> getAllDoor(){
 		return exitList;
 	}
