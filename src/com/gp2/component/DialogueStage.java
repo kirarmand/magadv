c1df36ad8c5beda6e0e1c82fd2b852720a741886
moi <kirarmand@gmail.com>
2016-11-30 11:41:05 +0100

    ajout des class stageExitJail

--- src/com/gp2/component/DialogueStage.java
+++ src/com/gp2/component/DialogueStage.java
@@ -2,6 +2,9 @@ package com.gp2.component;
 
 public abstract class DialogueStage {
 
+    /*
+    * class who give the basics answers of the NPC
+    */
     private String Sentence;
 
     public DialogueStage(String sentence) {
--- src/com/gp2/component/DialogueStageChoice.java
+++ src/com/gp2/component/DialogueStageChoice.java
@@ -4,6 +4,11 @@ import jdk.nashorn.internal.ir.IfNode;
 
 import java.util.ArrayList;
 
+/*
+ * class which inherits from the DialogueStage class
+ * Which can give a precise object after the sentence
+ */
+
 public class DialogueStageChoice extends DialogueStage {
 
     private ArrayList <String> answers;
